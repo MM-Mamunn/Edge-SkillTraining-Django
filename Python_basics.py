eabdb4a33c2f5c8e5c38212378691363bee9ffd7
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2024-09-13 12:42:14 +0600

    Create Python_basics.py

---
+++ Python_basics.py
@@ -0,0 +1,80 @@
+#Task 1 -  Variable naming
+# # take inputs
+# name = input("Enter Your name: ")
+# age = int(input("Enter Your Age: "))
+# fav_color = input("Enter your favourite colour: ")
+
+# #outputs 
+# print(f"My name is {name} and I am {age} years old. My favourite colour is {fav_color}.")
+
+#Task : 2 - Take two inputs and add them
+# take inputs of two integers
+# num_1 = int(input("Enter the First integer: "))
+# num_2 = int(input("Enter the Second integer: "))
+
+# #calculation
+# result = num_1 + num_2
+
+# #output the result
+# print("The Result is ",result)
+
+
+# #Task :3 - String Manupulation
+
+# strng = "my name is "
+# name = "Mamun Mahmud"
+
+# #concat two string
+# full_text = strng + name
+# print(full_text)
+
+# #convert to uppercase
+# print(f"converted the string ({full_text}) to upper {full_text.upper()}")
+
+# #Capitalization
+# print(f"Capitalize {full_text.capitalize()}")
+
+# #substring by slicing
+# print(f"The name is {full_text[11:len(name)]}")
+
+# #replace 
+# new_name = name.replace("Mamun Mahmud","Najmus Sakib")
+# print(f"{name} is replaced, New name is {new_name}")
+
+# #find starting index
+# index = full_text.find("Mamun")
+# print(f"find index {index}")
+
+# #Checking  starts with 
+# starts_with= strng.startswith("my")
+# print(f"starts with my is {starts_with}")
+
+# #convert to list
+# char_list = list(name)
+# print(f"{name} is divided in list, The list is ",char_list)
+
+# #join from a list 
+# joined_list = ''.join(char_list)
+# print(f"The {char_list} is joined that is {joined_list}")
+
+# #repeat a string
+# print(f"Repeat the name  three times {name  * 3}")
+
+# #task : 5 - boolean operators
+# a = 4
+# b = 5
+# print(f"a is {a} and  b is {b}")
+
+# #increase a by one if b == 5
+# b += (a == 4)
+# print(f"increase b by one if b == 5 a is {a} and b is {b}")
+
+# #check a and b are equal or not
+# print("a and b are equal", a == b )
+
+# #check a and b are equal or not
+# print("a and b are not equal", a != b )
+
+# #print the greatest number
+# print("a is greater than b" , a >  b)
+
