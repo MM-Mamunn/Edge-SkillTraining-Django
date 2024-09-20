b27346d39fe14b9e37ea49872e084cb19a5d32c3
MM-Mamunne <gamesmamun8@gmail.com>
2024-09-20 12:23:18 +0600

    symmatric diff

--- C3_Functions/ts3_C221046_t5.py
+++ C3_Functions/ts3_C221046_t5.py
@@ -7,5 +7,7 @@ set2 = {"Rayhan","Rafi","Nahian"}
 union_set = set1 | set2
 intersection = set1 & set2
 diff = (set1 - set2) | (set2 - set1)
+# this diff also can be done like this
+diff = set1 ^ set2  # set1 ^ set2  is same as  (set1 - set2) | (set2 - set1)
 print(f"The students in both sets are {intersection}")
 print(f"The students in one sets are {diff}")
