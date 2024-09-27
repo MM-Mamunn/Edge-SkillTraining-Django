f4772a46a14d96c297e7e51176231a2b53e2f04d
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2024-09-27 12:20:51 +0600

    Update TS_05_C221046_08.py

--- C5_inheritance/TS_05_C221046_08.py
+++ C5_inheritance/TS_05_C221046_08.py
@@ -1,5 +1,5 @@
 # Dependancy inversion ( SOLID -> D)
-# This ensures that low level(parent) class must not dependent of high(child) level class
+# This ensures that High level(parent) class must not dependent of Low (child) level class
 
 
 # It violates the single dependency ('S'OLID) as the notification is using to send email.Notification must serve a single task
@@ -14,3 +14,27 @@ class Notification:
 
     def notify(self, message):
         self.service.send(message)
+        
+
+# so the correct code is 
+class NotificationService:
+    def send(self, message):
+        pass
+
+class EmailService(NotificationService):
+    def send(self, message):
+        print(f"Sending email: {message}")
+
+class Notification:
+    def __init__(self, service: NotificationService):
+        self.service = service
+    
+    def notify(self, message):
+        self.service.send(message)
+
+# Usage
+email_service = EmailService()
+notification = Notification(email_service)
+notification.notify("Hello!")
+        
+        
