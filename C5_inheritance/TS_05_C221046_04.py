19f64bb5d893f3935a33a0133f58802fa8765d08
Mamun Mahmud <108848464+MM-Mamunn@users.noreply.github.com>
2024-09-27 11:32:57 +0600

    Update TS_05_C221046_04.py

--- C5_inheritance/TS_05_C221046_04.py
+++ C5_inheritance/TS_05_C221046_04.py
@@ -1,6 +1,6 @@
 # Abstraction is like template class that defines what methods or shape the child classes will look like
 # ABC and abstractmethod means that all the child of parent(Player) must have the "play" method defined,otherwise its an error
-# alternative of this is in the next tast "TS_05....05.py" 
+# alternative of this is in the next tast "TS_05.C221046.05.py" (alternative)
 from abc import ABC,abstractmethod
 
 class Player(ABC):
